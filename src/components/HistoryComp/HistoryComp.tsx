aed71839721554c79d77ca0b560b3cbee67532dd
Atabask <aleksanin1396@mail.ru>
2023-02-06 09:11:59 +0300

    Сделано вывод истории через .map
    НУЖНО ПРОТИПИЗИРОВАТЬ

--- src/components/HistoryComp/HistoryComp.tsx
+++ src/components/HistoryComp/HistoryComp.tsx
@@ -1,6 +1,8 @@
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import React, { FC, useEffect, useState } from 'react';
+import supabase from '../../supabaseClient';
 import './HistoryCompStyle.css'
+import { HistoryCard } from './HistoryDataCard/HistoryCard';
 
 
 
@@ -8,39 +10,35 @@ import './HistoryCompStyle.css'
 
 export const HistoryComp: FC = () => {
 
-    const supabaseClient = useSupabaseClient()
-    const [dataCommunalService, setDataCommunalService] = useState<string[]>([])
-
-    const getDataCommunalService = async () => {
-        try {
-            const { data, error } = await supabaseClient
-                .from('communal_service')
-                .select(`*`)
-                .limit(10)
-            if (data != null) {
-                setDataCommunalService(data)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
+    const [dataCommunalService, setDataCommunalService] = useState(null)
+    
     useEffect(() => {
-        getDataCommunalService()
+        const getHistoryDataCommunalService = async () => {
+            const { data: {user} } = await supabase.auth.getUser()
+            const { data, error } = await supabase
+            .from('communal_service')
+            .select('*')
+            .eq('user_id', user?.id)
+
+            if(error) throw error
+            if(data) {
+                setDataCommunalService(data)            //ПРОПИСАТЬ ТИПЫ
+            }
+        }
+        getHistoryDataCommunalService()
     }, [])
+    
 
     return (
         <div className='conteiner_history'>
-            <h1>Вывод через .map показаний за месяца</h1>
-            {dataCommunalService && (
-                    <div>
-                        {dataCommunalService.map(data => (
-                            <div >
-                                <p>{data}</p>
-                            </div>
-                        ))}
-                    </div>
-                )}
+            <h1>История показаний</h1>
+            { dataCommunalService && (
+                <div className='conteiner_card'>
+                    {dataCommunalService.map(data => (
+                        <HistoryCard  data={data}/>
+                    ))}
+                </div>
+            ) }
         </div>
     )
 
---
+++ src/components/HistoryComp/HistoryDataCard/HistoryCard.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import './HistoryCardStyle.css'
+
+interface IDataProps {
+    cold_water: string,
+    hot_water: string,
+    electro: string,
+    month: string,
+    year: string,
+    id: number,
+    user_id: string
+}
+
+interface IProps {
+    data: IDataProps
+}
+
+
+
+export const HistoryCard: FC<IProps> = ({ data }: IProps) => {
+
+    return (
+        <div className="conteiner">
+            <h2 className="card_title">Год: {data.year}</h2>
+            <h2 className="card_title">Месяц: {data.month}</h2>
+            <h2 className="card_title">Электроэнергия: {data.electro}КВт</h2>
+            <h2 className="card_title">Горячая воды:{data.hot_water}Кубов</h2>
+            <h2 className="card_title">Холодная вода:{data.cold_water}Кубов</h2>
+        </div>
+    )
+}  
